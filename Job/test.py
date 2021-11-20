f4743056432a76bb3a52e34d72d13fca9e903f71
jangjh0201 <jangjh0201@gmail.com>
2021-11-20 20:04:44 +0900

    delete : test.py

--- Job/test.py
+++
@@ -1,7 +0,0 @@
-import logging
-import time
-from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
-
-req_proxy = RequestProxy()
-proxies = req_proxy.get_proxy_list()
-proxies
